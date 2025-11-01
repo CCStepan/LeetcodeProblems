550f38f722a6ce071cd8ce95a5012eeaa47dda30
Stepan Chubakov <chubakovstepan1@gmail.com>
2025-11-01 22:27:06 +0300

    add 2 problmes

---
+++ ValidAnagram/Solution.java
@@ -0,0 +1,26 @@
+package ValidAnagram;
+
+class Solution {
+
+
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] letters1 = new int[26];
+        int[] letters2 = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            letters1[s.charAt(i) - 97]++;
+            letters2[t.charAt(i) - 97]++;
+        }
+        for (int i = 0; i < 26; i++) {
+
+            if (letters1[i] != letters2[i]) {
+                return false;
+            }
+        }
+
+
+        return true;
+    }
+}
